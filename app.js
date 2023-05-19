1e275c9fee24c3d383e113e0fb61306122b3091b
ETH1Elohim <ezmax.defi@gmail.com>
2023-05-19 10:07:47 -0500

    post page done

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cookieSession({
 //routes 
 app.use(require('./routes/index.js'))
 app.use(require('./routes/profile.js'))
+app.use(require('./routes/newpost.js'))
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
