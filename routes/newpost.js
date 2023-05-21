bfec7575a6b21e8406243f6e0bf99e20d7efa23b
Hunter Atkins <lsatkins111@gmail.com>
2023-05-21 17:27:14 -0500

    mostly finished with creating posts

--- routes/newpost.js
+++ routes/newpost.js
@@ -7,11 +7,48 @@ const auth = require('../auth'); //auth/index.js
 
 router.get('/newpost', auth, async (req, res) => {
     try{
-        res.render('newpost')
+        let exerciseNames = await db.exercises.findAll({attributes: ['id','name']});
+        
+        let arrOfObjs = [];
+        exerciseNames.forEach(exercise => {
+
+            let obj = {};
+
+            obj["id"] = exercise.dataValues.id;
+            obj["name"] = exercise.dataValues.name;
+            arrOfObjs.push(obj);
+    })
+
+        console.log(arrOfObjs);        
+        res.render('newpost', {
+            exercises: arrOfObjs
+        })
     }
     catch (error){
         console.log(error);
         res.render('newpost')
+    }
+})
+
+router.post('/newpost', auth, async (req, res) => {
+    try{
+        let userID = req.session.passport.user;
+        let {exerciseID, sets, reps, weight, minutes, seconds, distance, measurement, calories, notes} = req.body;
+
+        sets = parseInt(sets);
+        reps = parseInt(reps);
+        weight = parseInt(weight);
+
+        console.log(userID, 'userID');
+        console.log(req.body);
+
+        await db.posts.create({userID, exerciseID, sets, reps, weight, minutes, seconds, distance, measurement, calories, notes});
+
+        res.redirect('/');
+    }
+    catch (error){
+        console.log(error);
+        res.redirect('/newpost')
     }
 })
 
